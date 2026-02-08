e86294271cbb313f2b0bea8c8179070a217d3000
annabeserra <bluseraccess@gmail.com>
2026-02-08 17:06:52 -0300

    Added def to githu api call logs, tested docker containers.

--- projetos/data_engineering/credit_guard_ml_pipeline/scripts/main.py
+++ projetos/data_engineering/credit_guard_ml_pipeline/scripts/main.py
@@ -8,6 +8,9 @@ from google.cloud import storage
 from datetime import datetime
 from pathlib import Path
 import pytz
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # config base directory
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,9 +23,13 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = KEY_PATH
 INPUT_PATH = os.path.join(BASE_DIR, 'data', 'input', 'cnpjs.csv')
 RAW_DATA_PATH = os.path.join(BASE_DIR, 'data', 'raw')
 os.makedirs(RAW_DATA_PATH, exist_ok=True)
+GITHUB_DATA_PATH = os.path.join(BASE_DIR, 'data', 'github')
+os.makedirs(GITHUB_DATA_PATH, exist_ok=True)
 
 CLIENT = storage.Client()
 BUCKET = CLIENT.bucket('credit-guard-raw-sa-east1')
+
+GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
 
 def buscar_cnpj(cnpj):
     cnpj= re.sub(r'\D', '', str(cnpj))
@@ -63,6 +70,36 @@ def data_injection(file_name):
 
     print(f'file {file_name} successfully uploaded')
 
+#github api call
+def get_github_workflow():
+    owner = 'ANIBEserra'
+    repo = 'Portifolio' 
+    workflow_id = '231418862'
+    headers = {'Authorization': f'token {GITHUB_TOKEN}', 'Accept': 'application/vnd.github.v3+json'}
+
+    url_runs = f"https://api.github.com/repos/{owner}/{repo}/actions/workflows/{workflow_id}/runs?per_page=1"
+    
+    try:
+        # Get the last run ID
+        response = requests.get(url_runs, headers=headers)
+        if response.status_code == 200:
+            run_data = response.json()
+            run_id = run_data['workflow_runs'][0]['id']
+
+        # Job Details
+        url_jobs = f"https://api.github.com/repos/{owner}/{repo}/actions/runs/{run_id}/jobs"
+        response = requests.get(url_jobs, headers=headers)
+        if response.status_code == 200:
+            jobs_data = response.json()
+            file_path = os.path.join(GITHUB_DATA_PATH, f"github_workflow_run.json")
+
+            with open(file_path, 'w', encoding='utf-8') as f:
+                json.dump(jobs_data, f, indent=4, ensure_ascii=False)
+            return jobs_data
+
+    except Exception as e:
+        print(f"Error: {e}")
+
 
 df_input = pd.read_csv(INPUT_PATH, dtype={'CNPJ': str})
 for cnpj in df_input['CNPJ']:
@@ -75,3 +112,6 @@ for file in local_files:
         data_injection(file)
     else:
         pass
+
+get_github_workflow()
+
